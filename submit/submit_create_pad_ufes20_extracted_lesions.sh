df249d8db7dbec9e785c759ca6f6a2f339ad4483
tnroemer <99101377+tnroemer@users.noreply.github.com>
2026-05-07 17:30:17 -0700

    change create data to CPU

--- submit/submit_create_pad_ufes20_extracted_lesions.sh
+++ submit/submit_create_pad_ufes20_extracted_lesions.sh
@@ -2,14 +2,13 @@
 #SBATCH --job-name=create_pad_ufes20_extracted_lesions
 #SBATCH --output=/dev/null
 #SBATCH --error=/dev/null
-#SBATCH --partition=GPU-shared
-#SBATCH --gres=gpu:1
+#SBATCH -p RM-shared
 #SBATCH --account=mth250011p
-#SBATCH --nodes=1
+#SBATCH -N 1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=20G
-#SBATCH --time=03:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=1900M
+#SBATCH --time=01:00:00
 
 set -euo pipefail
 
